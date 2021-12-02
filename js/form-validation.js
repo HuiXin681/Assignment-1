1cb3e22ab4dd7571baccbc760f77deef27c34dcf
HuiXin681 <s10223047@connect.np.edu.sg>
2021-12-02 22:13:22 +0800

    put js file in a folder and made some tweaks

---
+++ js/form-validation.js
@@ -0,0 +1,44 @@
+let checkboxes = document.querySelectorAll('input[type="checkbox"]');
+let checked = false;
+let nameGiven = false;
+let emailGiven = false;
+    
+function submit() {
+    if (document.querySelector('input[type="text"]').value.length != 0)
+    {
+        nameGiven = true;
+    }
+
+    if (document.querySelector('input[type="email"]').value.length != 0)
+    {
+        emailGiven = true;
+    }
+
+    for (let i = 0; i<checkboxes.length; i++){
+        if(checkboxes[i].checked)
+        {
+            checked = true;
+            break;
+        }
+    }
+
+    if (checked == true && nameGiven == true && emailGiven == true)
+    {
+        window.alert("Submission complete");
+       
+    } 
+
+    else if (nameGiven != true)
+    {
+        window.alert("Please enter name");
+    }
+    else if (emailGiven != true)
+    {
+        window.alert("Please enter email");
+    }
+    else if (checked != true)
+    {
+        window.alert("Please select at least one area to be updated on");   
+    }
+
+}
